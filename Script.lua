2d8801d2b80a6c2ba6861821f64a9c883fb85859
maconhacompresunto230-design <maconhacompresunto230@gmail.com>
2026-09-08 03:14:11 -0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -7,7 +7,7 @@ local mouse = player:GetMouse()
 -- CONFIGURAÇÃO DA MIRA
 -- =========================================================
 
-local WHITE_CROSSHAIR = "rbxassetid://104385605798041"
+local WHITE_CROSSHAIR = "rbxassetid://79617922719488"
 local TARGET_TOOL_NAME = "Gun"
 
 local ATIVADO = false -- A mira liga sozinha ao pegar a arma
